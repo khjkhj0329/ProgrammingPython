dca4a5a72fb3de70c92404efbf68f5ff4f96a8e3
효정이 <w2029@e-mirim.hs.kr>
2021-05-07 08:46:46 +0900

    파이썬 수업시간

--- nadocoding/2308_quiz.py
+++ nadocoding/2308_quiz.py
@@ -189,63 +189,76 @@
 # print(bmi) #22.3
 print("----3번째 시간----")
 # Quiz3-1. n_sum() 함수를 만든다. 함수의 인수(argument)로 10자리 숫자보다 작은 숫자를 넣으면, 각 자리의 숫자의 합계를 리턴한다. 10자리 이상이면 -1 리턴한다.
-# def n_sum(argument):
-#     result = 0
-#     if((len(str(argument))) <= 10):
-#         for i in str(argument):
-#             result = result + int(i)
-#         return result
-#     elif((len(str(argument))) >= 10):
-#         print(-1)
-# result = n_sum(10)
-# print(result)
-# result = n_sum(331)
-# print(result)
-# result = n_sum(408)
-# print(result)
-# result = n_sum(1000000000)
-# print(result)
-# result = n_sum(10)
-# print(result)        #1
-# result = n_sum(331)
-# print(resuresult = n_sum(1000000000)
-# # print(result)lt)         #7
-# result = n_sum(408)
-# print(result)         #12
-# result = n_sum(1000000000)
-# print(result)         #-1
+def n_sum(argument):
+    result = 0
+    num = str(argument)
+    if len(num) >= 10:
+        return -1
+    # for i in range(len(num)):       #len('408'): 3 range(3): i: 0 1 2
+    #     result += int(num[i])       #num[0]: '4' num[1]: '0' num[2]: '8'
+    for i in num:
+        result += int(i)
+    return result
+result = n_sum(10)
+print(result)
+result = n_sum(331)
+print(result)
+result = n_sum(408)
+print(result)
+result = n_sum(1000000000)
+print(result)
+# # result = n_sum(10)
+# # print(result)        #1
+# # result = n_sum(331)
+# # print(resuresult = n_sum(1000000000)
+# # # print(result)lt)         #7
+# # result = n_sum(408)
+# # print(result)         #12
+# # result = n_sum(1000000000)
+# # print(result)         #-1
+#
+# # Quiz3-2. get_subway_fare() 함수를 만든다. 이 함수는 인수로 가는 거리(km)를 숫자로 넣으면, 요금을 리턴한다.
+# # * 지하철 요금계산법 10km 이내: 720원(청소년), 10~50km: 5km마다 100원, 50km 초과 시 8km마다 100원
+#
+def get_subway_fare(fare):
+    pay = 720
 
-# Quiz3-2. get_subway_fare() 함수를 만든다. 이 함수는 인수로 가는 거리(km)를 숫자로 넣으면, 요금을 리턴한다.
-# * 지하철 요금계산법 10km 이내: 720원(청소년), 10~50km: 5km마다 100원, 50km 초과 시 8km마다 100원
-# def get_subway_fare(fare):
-#     value = 0
-#     if(10>fare):
-#         print("720원")
-#     elif(10<=fare<50):
-#         print(value+100)
-#     elif(50>fare):
-#         print()
-# fare = get_subway_fare(5)
-# print(fare)        #720
-# fare = get_subway_fare(26)
-# print(fare)        #1120
-# fare = get_subway_fare(61)
-# print(fare)        #1720
+    if (fare <= 10):
+        return pay
+    elif (10 < fare < 50):
+        for fare in range(10, fare, 5):
+            pay += 100
+        return pay
+    elif 50 < fare:
+        pay = 1520
+        for fare in range(50, fare, 8):
+            pay += 100
+        return pay
+fare = get_subway_fare(5)
+print(fare)        #720
+fare = get_subway_fare(10)
+print(fare)        #720
+fare = get_subway_fare(25)
+print(fare)        #1020
+fare = get_subway_fare(26)
+print(fare)        #1120
+fare = get_subway_fare(61)
+print(fare)        #1720
+#
+# # Quiz3-3. get_three_six_nine() 함수를 만든다. 이 함수에 숫자를 입력하면 369 게임에  해당하는 답변을 리턴한다.
+# # * 369게임: 숫자의 어느 자리든 3 또는 6 또는 9가 있다면 그 갯수만큼 '짝'을 외치고, 아니면 그냥 숫자를 외친다.
+def get_three_six_nine(num):
+    number = str(num)
+    result = ""
 
-# Quiz3-3. get_three_six_nine() 함수를 만든다. 이 함수에 숫자를 입력하면 369 게임에  해당하는 답변을 리턴한다.
-# * 369게임: 숫자의 어느 자리든 3 또는 6 또는 9가 있다면 그 갯수만큼 '짝'을 외치고, 아니면 그냥 숫자를 외친다.
-def get_three_six_nine(result):
-    f = int(result/10);
-    s = int(result%10);
-    if(f%3==0 and s%3 == 0):
-        if(f%3 == 0 or f != 0):
-            print("짝")
-            if(s%3 == 0 or s != 0):
-                print("짝")
-    elif(s%3 ==0 or s != 0):
-        print("짝")
+    count = (number.count('3') + number.count('6') + number.count('9'))
+    if count == 0:
+        result = number
     else:
-        print("없음")
+        # for i in range(count):
+        #     result += '짝'
+        result = count * "짝"
+    return result
 result = get_three_six_nine(1)
 print(result)        #1
 result = get_three_six_nine(3)
@@ -264,6 +277,40 @@ print(result)        #짝짝
 # print(result)        #짝짝
 
 # 'Quiz3-4. 나만의 재미난 문제를 만들어보세요. 단, 조건이 있습니다.
+'''
+# 함수의 이름을 num_gender으로 정하고 (resident_num)인수를 선언을 한다. 인수 안에는 주민번호 14자리를 입력하게 한다.
+# 그리고 성별을 확인을 위해 성별을 리턴을 한다.
+# num_gender(040329-4123456)
+# print(result)  #여자입니다.
+# num_gender(040329-2123456)
+# print(result) #여자입니다.
+# num_gender(040809-18495064)
+# print(result) #남자입니다.
+# num_gender(040809-38495064)
+# print(result) #남자입니다.
+# num_gender(040402-90459349)
+# print(result) #등록된 번호가 아닙니다.
+'''
+def num_gender(resident_num):
+    if (resident_num[7] == '2') or (resident_num[7] == '4'):
+        result = '여자입니다.'
+    elif (resident_num[7:8] == '1') or (resident_num[7:8] == '3'):
+        result = '남자입니다.'
+    else:
+        result = '등록된 번호가 아닙니다.'
+    return result
+
+gender = num_gender('040329-4123456')
+print(gender) #여자입니다.
+gender =num_gender('040329-2123456')
+print(gender) #여자입니다.
+gender =num_gender('040809-18495064')
+print(gender) #남자입니다.
+gender =num_gender('040809-38495064')
+print(gender) #남자입니다.
+gender = num_gender('040402-90459349')
+print(gender) #등록된 번호가 아닙니다.
+
 # 1. 함수의 이름을 정해준다.
 # 2. 인수로 넣어야하는 자료형이나 개수를 말해준다.
 # 3. 리턴하는 것을 말해준다.
@@ -271,4 +318,62 @@ print(result)        #짝짝
 # 5. 내가 낸 문제의 답안을 제출한다.
 
 
+# '''Quiz4-1. [학생퀴즈] 소수판별하기(소수: 1이나 자기자신으로만 나누어 떨어지는 수)
+# is_prime() 함수를 만든다.
+# 인수로 1개의 숫자를 받는다.
+# 인수로 넘어온 숫자가 소수(prime number)이면 "소수" 아니면, "소수 아님" 리턴한다.
+# '''
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            result = '소수 아님'
+        else:
+            result = '소수'
+        return result
 
+result = is_prime(2)
+print(result)     #소수
+result = is_prime(13)
+print(result)     #소수
+result = is_prime(36)
+print(result)     #소수 아님
+# def is_prime_number(x):
+#     # 2부터 (x - 1)까지의 모든 수를 확인
+#     for i in range(2, x):
+#         # x가 해당 수로 나누어떨어지면
+#         if x % i == 0:
+#             return '소수 아님'
+#     return '소수'
+
+
+# print(is_prime_number(4))
+# print(is_prime_number(2))# False
+
+
+'''
+# Quiz4-2. [학생퀴즈] get_compliment() 함수를 만든다. 이 함수에 '고구마' 또는 '맛있는'이 들어가는 말을 입력하면 그에 해당하는 답변을 리턴한다.
+# '고구마'가 들어가는 말을 입력하면 '왓쇼이!', '맛있는'이 들어가는 말을 입력하면 '우마이!',
+# '놀랄 만한', '황당한', '굉장한'이 들어가는 말을 입력하면 '요모야..!', 특정 단어가 하나라도 들어가지 않는다면 '으무!'를 리턴한다. 
+# '''
+def get_compliment(japan):
+    if "고구마" in japan:
+        return "왓쇼이!"
+    elif "맛있는" in japan:
+        return "우마이!"
+    elif "놀랄 만한" in japan:
+        return "오묘야!"
+    elif "황당한" in japan:
+        return "오묘야!"
+    elif "굉장한" in japan:
+        return "오묘야!"
+    else:
+        return "으무!"
+
+result = get_compliment('고구마 된장국')
+print(result) # 왓쇼이!
+result = get_compliment('맛있는 크레이프')
+print(result) # 우마이!
+result = get_compliment('놀랄 만한 상황')
+print(result) # 요모야..!
+result = get_compliment('좋은 마음가짐이다!')
+print(result) # 으무!
